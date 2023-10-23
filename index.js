18a566d501676ae3cf29dfba383f1a71b875e6b4
Nikolay Zlobin <zlobinprod@yandex.ru>
2023-10-23 18:46:37 +0300

    perviy

---
+++ index.js
@@ -0,0 +1,121 @@
+const startBtn = document.querySelector('.startBtn');
+const mainStartBtn = document.querySelector('.mainStartBtn');
+
+const lapResetBtn = document.querySelector('.lapResetBtn');
+const mainLapBtn = document.querySelector('.mainLapBtn');
+
+const laps = document.querySelector('.laps');
+
+let milisec = 0;
+let sec = 0;
+let min = 0;
+
+let m = 0;
+let s = 0;
+let ms = 0;
+
+let time = document.querySelector('.time');
+let int = null;
+
+let isTimerStarted = false;
+
+let allLaps = [];
+let lapsNumber = 1;
+
+startBtn.addEventListener('click', () => {
+    if(isTimerStarted === false) {
+        int = setInterval(displayTimer, 10);
+    } else {
+        clearInterval(int);
+    }
+    changeStartBtn();
+    checkIsResetAvailable();
+})
+
+function displayTimer() {
+    milisec++;
+
+    if(milisec >= 99) {
+        sec++;
+        milisec = 0;
+    }
+    if(sec >= 60) {
+        min++;
+        sec = 0
+    }
+
+    m = min < 10 ? "0" + min : min;
+    s = sec < 10 ? "0" + sec : sec;
+    ms = milisec < 10 ? "0" + milisec : milisec;
+
+    time.innerHTML = `${m}:${s},${ms}`;
+
+}
+
+function changeStartBtn() {
+   if(isTimerStarted === false) {
+    isTimerStarted = true;
+
+    startBtn.innerHTML = "Стоп";
+    startBtn.classList.add("timerStarted");
+    mainStartBtn.classList.add("timerStartedMain");
+   } else {
+        isTimerStarted = false;
+    
+    startBtn.innerHTML = "Старт";
+    startBtn.classList.remove("timerStarted");
+    mainStartBtn.classList.remove("timerStartedMain");
+   }
+}
+
+function checkIsResetAvailable() {
+    if(isTimerStarted === false) {
+        lapResetBtn.innerHTML = "Сброс";
+    } else {
+        lapResetBtn.innerHTML = "Круг";
+    }
+}
+
+function displayLaps() {
+    lapsNumber++;
+    laps.innerHTML = ""
+    if(allLaps.length > 0) {
+        allLaps.map(item => {
+            laps.innerHTML += `
+                <div class="lap">
+                    <span>Круг ${item.number}</span>
+                    <span>${item.time}</span>
+                    </div>
+            `
+        })
+    }
+}
+
+lapResetBtn.addEventListener('click', () => {
+    if(isTimerStarted === false) { //Reset click
+        clearInterval(int);
+        m = 0;
+        s = 0;
+        ms = 0;
+
+        milisec = 0;
+        sec = 0;
+        min = 0;
+        time.innerHTML = "00:00,00";
+
+        allLaps = [];
+        laps.innerHTML = "";
+        lapsNumber = 1;
+        
+    } else { // Lap Click
+        allLaps.push(
+            {
+                time: m + ":" + s + "," + ms,
+                number: lapsNumber
+            }
+        )
+
+        displayLaps();
+        console.log(allLaps);
+    }
+})
